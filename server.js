57748d7917700f23f0ad7ab51a183f0c462a512b
Shahzaib Ali <shahzaibalisomroo@gmail.com>
2025-07-01 09:43:55 +0500

    setting up socket.io in next.js app router

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ import next from "next";
 import { Server } from "socket.io";
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
-// when using middleware `hostname` and `port` must be provided below
+const hostname = process.env.HOSTNAME;
+const port = process.env.PORT;
+
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
