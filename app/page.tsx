298ca3630d1c80c1e81ceea5b607d7dd4669d42d
Shahzaib Ali <shahzaibalisomroo@gmail.com>
2025-07-01 23:12:57 +0500

    created header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,47 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { socket } from "@/socket";
+// import { useEffect, useState } from "react";
+// import { socket } from "@/socket";
 import Board from "@/components/layout/Board";
+import Header from "@/components/layout/Header";
 
 export default function Home() {
-  const [isConnected, setIsConnected] = useState(false);
-  const [transport, setTransport] = useState("N/A");
+  // const [isConnected, setIsConnected] = useState(false);
+  // const [transport, setTransport] = useState("N/A");
 
-  useEffect(() => {
-    if (socket.connected) {
-      onConnect();
-    }
+  // useEffect(() => {
+  //   if (socket.connected) {
+  //     onConnect();
+  //   }
 
-    function onConnect() {
-      setIsConnected(true);
-      setTransport(socket.io.engine.transport.name);
+  //   function onConnect() {
+  //     setIsConnected(true);
+  //     setTransport(socket.io.engine.transport.name);
 
-      socket.io.engine.on("upgrade", (transport) => {
-        setTransport(transport.name);
-      });
-    }
+  //     socket.io.engine.on("upgrade", (transport) => {
+  //       setTransport(transport.name);
+  //     });
+  //   }
 
-    function onDisconnect() {
-      setIsConnected(false);
-      setTransport("N/A");
-    }
+  //   function onDisconnect() {
+  //     setIsConnected(false);
+  //     setTransport("N/A");
+  //   }
 
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
+  //   socket.on("connect", onConnect);
+  //   socket.on("disconnect", onDisconnect);
 
-    return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-    };
-  }, []);
-
+  //   return () => {
+  //     socket.off("connect", onConnect);
+  //     socket.off("disconnect", onDisconnect);
+  //   };
+  // }, []);
+  
   return (
     <div>
+      <Header/>
       <div>
-        <p>Status: { isConnected ? "connected" : "disconnected" }</p>
-        <p>Transport: { transport }</p>
-      </div>
-      <div>
-        <Board/>
+        {/* <Board /> */}
       </div>
     </div>
   );
